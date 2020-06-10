287f79bfee9cf3c410575b444cae5a3913dca64e
Ilya <50113149+Sukhov63@users.noreply.github.com>
2020-06-11 00:27:20 +0400

    Initial

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+// We need the UnityEngine.XR.WSA namespace to access Holographic Settings which contain 
+// functions which effect the performance and presentation of Holograms on Windows 
+// Holographic platforms.
+using UnityEngine.XR.WSA;
+
+public class GameManager : MonoBehaviour
+{
+
+    void Start()
+    {
+        // Check if the MR headset display is transparent (not opaque).
+        if (!HolographicSettings.IsDisplayOpaque)
+        {
+            // If that is the case, then we need to hide the virtual living room
+            GameObject[] HMDUOnlyObjects = GameObject.FindGameObjectsWithTag("HMDUOnly");
+            for (int i = 0; i < HMDUOnlyObjects.Length; i++)
+            {
+                // Set active to false disables the object.
+                HMDUOnlyObjects[i].SetActive(false);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SpeakerMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.XR.WSA;
+// namespace for the TwoHandManipulatable script
+using HoloToolkit.Unity.InputModule.Utilities.Interactions;
+// namespace for the TapToPlace script
+using HoloToolkit.Unity.SpatialMapping;
+
+public class SpeakerMovement : MonoBehaviour
+{
+
+    void Start()
+    {
+        if (!HolographicSettings.IsDisplayOpaque)
+        {
+            GetComponent<TwoHandManipulatable>().enabled = false;
+            GetComponent<TapToPlace>().enabled = true;
+        }
+    }
+}
